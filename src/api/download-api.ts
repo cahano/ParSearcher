a2f9c37b3737dc195d7e66bc5110dd1ec79eeab2
cahano <owencahan1@gmail.com>
2023-09-30 12:44:14 -0400

    reverting to OG dwnld structure + logging

--- src/api/download-api.ts
+++ src/api/download-api.ts
@@ -12,26 +12,29 @@ export async function FileDownload(url: string): Promise<void> {
 
     const res = await axios.get(url,
                                 { responseType: 'arraybuffer' })
-                                  .then((res) => { const outputFile = 'parse_results.xlsx';
-                                  console.log('FILE DONWLOAD DATA')
-                                  console.log(res.data.file)
-                              
-                                  // Blob object for storing output xlsx contents
-                                  const blob = new Blob([res.data.file])
-                              
-                              
-                                  // XLSX FILE IS CORRUPT - NEED TO FIX
-                                  // NEED TO DETERMINE IF TORNADO SIDE OR REACT SIDE FILE HANDLING
-                              
-                              
-                                  // Creating download link element
-                                  const URL = window.URL.createObjectURL(blob);
-                                  const output_el = document.createElement('a');
-                                  output_el.href = URL;
-                              
-                                  // Simulating link click
-                                  output_el.download = outputFile;
-                                  output_el.click(); })
+
+    console.log('FILE DONWLOAD DATA')
+    console.log(res.data.file)
+
+    // Blob object for storing output xlsx contents
+    const blob = new Blob([res.data.file],
+                          { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' },)
+
+
+
+    // XLSX FILE IS CORRUPT - NEED TO FIX
+    // NEED TO DETERMINE IF TORNADO SIDE OR REACT SIDE FILE HANDLING
+
+
+
+    // Creating download link element
+    const URL = window.URL.createObjectURL(blob);
+    const output_el = document.createElement('a');
+    output_el.href = URL;
+
+    // Simulating link click
+    output_el.download = 'parsed_output.xlsx';
+    output_el.click();
 
     return Promise.resolve();
 }
