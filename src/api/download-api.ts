c1b3cfe84fdcd5a5ed61f728fd824c7879ad281c
cahano <owencahan1@gmail.com>
2023-09-30 12:33:56 -0400

    attempting new file dwnld structure (removed 'fs')

--- src/api/download-api.ts
+++ src/api/download-api.ts
@@ -4,8 +4,6 @@
 
 import axios from 'axios';
 
-const fs = require('fs')
-
 export interface parsedFile {
     file: Blob
 }
@@ -16,32 +14,25 @@ export async function FileDownload(url: string): Promise<void> {
                                 { responseType: 'arraybuffer',
                                   headers: { 'Content-Type': 'application/vnd.openxmlformats-officedocument.spreedsheetml.sheet' } })
                                   .then((res) => { const outputFile = 'parse_results.xlsx';
-                                                   fs.writeFileSync(outputFile, res.data.file)
-                                                   return outputFile })
-
-
-
-    // console.log('FILE DONWLOAD DATA')
-    // console.log(res.data)
-
-    // // Blob object for storing output xlsx contents
-    // const blob = new Blob([res.data.file],
-    //                       { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'})
-
-
-
-    // // XLSX FILE IS CORRUPT - NEED TO FIX
-    // // NEED TO DETERMINE IF TORNADO SIDE OR REACT SIDE FILE HANDLING
-
-
-    // // Creating download link element
-    // const URL = window.URL.createObjectURL(blob);
-    // const output_el = document.createElement('a');
-    // output_el.href = URL;
-
-    // // Simulating link click
-    // output_el.download = 'parse_results.xlsx';
-    // output_el.click();
+                                  console.log('FILE DONWLOAD DATA')
+                                  console.log(res.data)
+                              
+                                  // Blob object for storing output xlsx contents
+                                  const blob = new Blob([res.data.file])
+                              
+                              
+                                  // XLSX FILE IS CORRUPT - NEED TO FIX
+                                  // NEED TO DETERMINE IF TORNADO SIDE OR REACT SIDE FILE HANDLING
+                              
+                              
+                                  // Creating download link element
+                                  const URL = window.URL.createObjectURL(blob);
+                                  const output_el = document.createElement('a');
+                                  output_el.href = URL;
+                              
+                                  // Simulating link click
+                                  output_el.download = outputFile;
+                                  output_el.click(); })
 
     return Promise.resolve();
 }
