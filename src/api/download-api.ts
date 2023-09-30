1dc0a6260bf6bae0ededfd20f63b9ccc3ba48d73
cahano <owencahan1@gmail.com>
2023-09-30 12:32:02 -0400

    attempting new file dwnld structure

--- src/api/download-api.ts
+++ src/api/download-api.ts
@@ -4,6 +4,8 @@
 
 import axios from 'axios';
 
+const fs = require('fs')
+
 export interface parsedFile {
     file: Blob
 }
@@ -13,29 +15,33 @@ export async function FileDownload(url: string): Promise<void> {
     const res = await axios.get(url,
                                 { responseType: 'arraybuffer',
                                   headers: { 'Content-Type': 'application/vnd.openxmlformats-officedocument.spreedsheetml.sheet' } })
-
-
-    console.log('FILE DONWLOAD DATA')
-    console.log(res.data)
-
-    // Blob object for storing output xlsx contents
-    const blob = new Blob([res.data.file],
-                          { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'})
+                                  .then((res) => { const outputFile = 'parse_results.xlsx';
+                                                   fs.writeFileSync(outputFile, res.data.file)
+                                                   return outputFile })
 
 
 
-    // XLSX FILE IS CORRUPT - NEED TO FIX
-    // NEED TO DETERMINE IF TORNADO SIDE OR REACT SIDE FILE HANDLING
+    // console.log('FILE DONWLOAD DATA')
+    // console.log(res.data)
+
+    // // Blob object for storing output xlsx contents
+    // const blob = new Blob([res.data.file],
+    //                       { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'})
 
 
-    // Creating download link element
-    const URL = window.URL.createObjectURL(blob);
-    const output_el = document.createElement('a');
-    output_el.href = URL;
 
-    // Simulating link click
-    output_el.download = 'parsed_output.xlsx';
-    output_el.click();
+    // // XLSX FILE IS CORRUPT - NEED TO FIX
+    // // NEED TO DETERMINE IF TORNADO SIDE OR REACT SIDE FILE HANDLING
+
+
+    // // Creating download link element
+    // const URL = window.URL.createObjectURL(blob);
+    // const output_el = document.createElement('a');
+    // output_el.href = URL;
+
+    // // Simulating link click
+    // output_el.download = 'parse_results.xlsx';
+    // output_el.click();
 
     return Promise.resolve();
 }
