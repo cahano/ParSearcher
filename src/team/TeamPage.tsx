9139e4f8cf00d98b563b6531ad589ca5bcdd8425
cahano <owencahan1@gmail.com>
2023-10-01 20:11:42 -0400

    updating degrees

--- src/team/TeamPage.tsx
+++ src/team/TeamPage.tsx
@@ -27,17 +27,17 @@ class TeamPage extends React.Component {
 
             <div className='member'>
               <h2>Brandon Huang</h2>
-              <p><i>Rensselaer Polytechnic Institute - Computer Science B.S. (2021); Grammarly, PNC.</i></p>
+              <p><i>Rensselaer Polytechnic Institute - Computer Science (2021); Grammarly, PNC.</i></p>
             </div>
 
             <div className='member'>
               <h2>Mo Hayat</h2>
-              <p><i>Rensselaer Polytechnic Institute - Electrical Engineering B.S. (2020); Bank of America, AWS.</i></p>
+              <p><i>Rensselaer Polytechnic Institute - Electrical Engineering (2020); Bank of America, AWS.</i></p>
             </div>
 
             <div className='member'>
               <h2>Owen Cahan</h2>
-              <p><i>Rensselaer Polytechnic Institute - Mathematics B.S., Business B.A. (2021); Magnetar, JP Morgan.</i></p>
+              <p><i>Rensselaer Polytechnic Institute - Mathematics, Business (2021); Magnetar, JP Morgan.</i></p>
             </div>
 
           </div>
