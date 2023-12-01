492f51e7fa2814e62883aafe7e8738b36d49cecd
cahano <owencahan1@gmail.com>
2023-11-30 22:11:25 -0500

    updating tornado URL to Brandon's local (HTTPS, new port)

--- src/constants.tsx
+++ src/constants.tsx
@@ -22,6 +22,6 @@ export const Pagefooter = () => {
 // URL Addresses for APIs //
 
 // URL for Brandon's local tornado
-export let API_URL:string = 'https://108.35.159.142:8888/'
+export let API_URL:string = 'https://108.35.159.142:443/'
 // URL for heroku tornado
 // export let API_URL:string = 'https://parsearcher-api-c4c5fa341782.herokuapp.com/' 
