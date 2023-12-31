9019da4c721dea2770d98f4326b36dad78283e25
MohaHayat <mohammadfhayat@gmail.com>
2023-12-31 12:11:24 -0500

    refactor axios request to send formdata only

--- src/api/upload-api.ts
+++ src/api/upload-api.ts
@@ -6,7 +6,6 @@ import axios from 'axios';
 
 export async function axiosPDFPost(formData: FormData, filename: string, url: string): Promise<void> {
 
-    console.log(formData)
     console.log(filename)
     console.log("Getting presinged URL from S3...")
     let operation = "get_s3_signed_url"
@@ -28,21 +27,7 @@ export async function axiosPDFPost(formData: FormData, filename: string, url: st
 
     console.log("S3 Response", certCall.data)
     
-    let signedURL: string = ""
-
-    // check if the response is a dictionary
-    if(typeof certCall.data == "object"){
-        signedURL = certCall.data["url"]
-        let fields = certCall.data["fields"]
-
-        // Append each field to formData
-        Object.entries(fields).forEach(
-            ([key, value]) => formData.append(key, <string>value)
-        );
-    }
-    else {
-        signedURL = <string>certCall.data 
-    }
+    let signedURL: string = <string>certCall.data 
 
 
     console.log("Form data: ", formData)
@@ -51,12 +36,7 @@ export async function axiosPDFPost(formData: FormData, filename: string, url: st
     let axiosMethod = operation == "get_s3_signed_url" ? "put" : "post"
 
     // Async post PDF data to AWS S3
-    await axios.put(signedURL, {
-        data: formData,
-        headers: {
-            "Content-Type": "application/pdf"
-        }
-    }).then((result) => {
+    await axios.put(signedURL, formData).then((result) => {
             console.log("S3 upload response: ", result.data);
         })
         .catch((error) => {
